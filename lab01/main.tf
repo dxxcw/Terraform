b8c0531d3d9130e94f9220cd83242f75de5f699f
dxxcw <hn01472@gmail.com>
2025-10-15 11:59:18 +0900

    [modify] lab01

--- lab01/main.tf
+++ lab01/main.tf
@@ -5,6 +5,8 @@
 # - Internet Gateway 생성 & 연결
 # - Public Subnet 생성
 # - Public Routing Table 생성 & Public Subnet 연결
+# - Security Group & Rule 생성
+# - EC2 생성
 ##################################################
 
 # 1. Provider 설정
@@ -67,4 +69,62 @@ resource "aws_route_table" "myPubRT" {
 resource "aws_route_table_association" "myPubRTassoc" {
   subnet_id      = aws_subnet.myPubSN.id
   route_table_id = aws_route_table.myPubRT.id
+}
+
+# 6. Security Group & Rule 생성
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "allow_myweb" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.myVPC.id
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+# Ingress Rule
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_security_group_ingress_rule
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_myweb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_myweb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+# Egress Rule
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_security_group_egress_rule
+resource "aws_vpc_security_group_egress_rule" "allow_outbound" {
+  security_group_id = aws_security_group.allow_myweb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+# 7. EC2 생성 
+resource "aws_instance" "myWEB" {
+  ami                    = "ami-077b630ef539aa0b5"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.myPubSN.id
+  vpc_security_group_ids = [aws_security_group.allow_myweb.id]
+
+  user_data_replace_on_change = true
+  user_data                   = <<-EOF
+    #!/bin/bash
+    yum -y install httpd
+    echo 'MyWEB' > /var/www/html/index.html
+    systemctl enable --now httpd
+    EOF
+
+  tags = {
+    Name = "myWEB"
+  }
 }
